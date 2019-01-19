ed20fc8dbc420286a93bd498a6caf39f9c0b9bb6
Kaushal Maganti <kmaganti@uwaterloo.ca>
2019-01-19 18:16:48 -0500

    add API endpoints

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+app.get("/api/test", (req, res) => {
+  res.status(200).json({"success": "yes"});
+}
+
