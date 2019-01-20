94b14573bad4a42fb21b7c8675c3c838863c5c84
Kaushal Maganti <kmaganti@uwaterloo.ca>
2019-01-19 23:13:31 -0500

    Fix bug

--- server.js
+++ server.js
@@ -2,7 +2,27 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const { Pool } = require('pg');
 
-import {isTrue, validateString, validateInt, validateMoney} from "./utils";
+// Checks if a string denotes true
+const isTrue = str => {
+  return str && (str === true || str === 'true' || str === 'True');
+};
+
+// Prevents SQL injections by limiting the symbol set for strings
+const validateString = str => {
+  return str && str.length > 0 && /^[a-zA-Z0-9-_,./?!#%&*()]*$/.test(str) && str;
+};
+
+// Checks that the input is a valid int within bounds
+const validateInt = (str, lower, upper) => {
+  const val = Number(str);
+  return val && val === Math.round(val) && val >= lower && (!upper || val <= upper) && val;
+};
+
+// Checks that the input is a valid amount of money within bounds
+const validateMoney = (str, lower, upper) => {
+  const val = Number(str) * 100;
+  return validateInt(val, lower * 100, upper * 100) && val / 100;
+};
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
